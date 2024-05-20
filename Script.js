14a10d098b13a93f91907382be44ad4afe6415e2
Matias Ricardo Carrillo Sanchez <matiascarrillo115@gmail.com>
2024-05-20 03:46:54 -0500

    added sum function

--- Script.js
+++ Script.js
@@ -5,9 +5,29 @@ if (nomb !==null && nomb !== "") {
     HelloElement.innerHTML = "<strong>Hola " + nomb + ", aqui informacion que te pueda interesar<strong>";
 }else{}
 }
+
 insertcontent()
+
+function solicitarnumero(){
+    const numero = prompt("Ingrese un numero");
+    return Number(numero);
+}
+
+function sumar(){
+    const numero1 = solicitarnumero();
+    const numero2 = solicitarnumero();
+    if (!isNaN(numero1) && !isNaN(numero2)){
+         return numero1 + numero2;
+    } else {
+        return"La suma no se pudo hacer. :c ";
+    }
+}
+
+const resultado = sumar();
+console.log("Resultado", resultado);
 
 //Function colorChange(){
     
-}
+//}
+
 
